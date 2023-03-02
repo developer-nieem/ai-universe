d4ca4d0353d9bee74112c52d3f9687f831a9fa69
developer-nieem <naimhossenwork@gmail.com>
2023-03-02 23:12:57 +0600

    create single post details

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // Get Api Data
 const getAiData = () => {
   const url = `https://openapi.programming-hero.com/api/ai/tools`;
@@ -12,9 +13,51 @@ getAiData();
 
 // display Api Data
 const displayAiData = (data) => {
-  console.log(data);
+
   const aiBlogsCard = document.getElementById("AiBlogsCard");
+
+
+
   data.forEach((singlePost) => {
-    console.log(singlePost);
+
+    //   feature list
+// const featurList =  document.getElementById('featurList');
+    // console.log(singlePost);
+    aiBlogsCard.innerHTML += `
+         <div class="col">
+              <div class="card h-100 p-2">
+                        <img src="${singlePost.image}" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">features</h5>
+                        <div id="featurList" class="card-text">   
+                            ${singlePost.features.map(feature =>{
+                                `${feature}`
+                            })}
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex  justify-content-between align-items-center">
+                        <div>
+                            <h4> ${singlePost.name}</h4>
+                            <i class="bi bi-calendar3"></i> <span>${singlePost.published_in}</span>
+                        </div>
+                        <div>
+                            <button onclick="loadAIPostDetails('${singlePost.id}')" class="rounded-circle border-0"><i class="bi bi-arrow-right"></i></button>
+                        </div>
+                    </div>
+              </div>
+         </div>
+    
+    `
   });
 };
+
+// load AI post details 
+const loadAIPostDetails = id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayAiData(data.data.tools))
+      .catch((error) => console.log(error));
+}
+
+
