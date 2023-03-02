2507edf7bc10d04de7d8fd1d35e2b4e76bb5d733
developer-nieem <naimhossenwork@gmail.com>
2023-03-02 21:35:09 +0600

    design post card

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
-
 // Get Api Data
-const getAiData =  () =>{
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayAiData(data.data.tools))
-    .catch(error => console.log(error));
-}
+const getAiData = () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAiData(data.data.tools))
+    .catch((error) => console.log(error));
+};
 
 // dicler function
 getAiData();
 
 // display Api Data
-const displayAiData = data => {
-console.log(data);
-}
+const displayAiData = (data) => {
+  console.log(data);
+  const aiBlogsCard = document.getElementById("AiBlogsCard");
+  data.forEach((singlePost) => {
+    console.log(singlePost);
+  });
+};
